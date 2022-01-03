f5e42252e8d0efa6d5e0f3734563745825ca8e91
Hugo Albert <96631538+hugoalbert03@users.noreply.github.com>
2022-01-03 02:59:04 -0300

    Atualização no código dos Emoji
    Foi alterado na linha 2 o enunciado da resposta e adicionado o "use_aliases=True
    
    O funcionamento teve êxito com a instalação do pip e do pacote de emoji no pip.

--- aula08a2.py
+++ aula08a2.py
@@ -1,2 +1,3 @@
 import emoji
-print(emoji.emojize("Olá, Mundo :sunglasses:"))
+print(emoji.emojize("Olá, Mundo :earth_americas:", use_aliases=True))
+print(emoji.emojize("Uso GitHub :rocket:", use_aliases=True))
