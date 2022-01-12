8a8547ca64f92bcece538834d4e394cd064f7fc1
Hugo Albert <96631538+hugoalbert03@users.noreply.github.com>
2022-01-12 02:55:13 -0300

    Exercício aula 09

---
+++ Aula09.py
@@ -0,0 +1,8 @@
+frase = 'Aula de Python'
+#print(frase.count('o'))
+#print(frase.replace('Aula','Curso'))
+#print(frase.find("Python"))
+#print(frase.lower().find('python'))
+div = frase.split()
+print(div[0])
+
