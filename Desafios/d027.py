93422fba6db1842ab180d1307b0ba7c937af26f1
Hugo Albert <96631538+hugoalbert03@users.noreply.github.com>
2022-01-18 00:10:33 -0300

    Acrescentando mais uma linha de comando

--- Desafios/d027.py
+++ Desafios/d027.py
@@ -1,4 +1,5 @@
 nome = str(input("Digite seu nome completo: ")).strip().split()
+print("Muito prazer te conhacer!")
 print("Primeiro nome = {}".format(nome[0]))
 vnome = int(len(nome)) - 1
 print("Último nome = {}".format(nome[vnome]))
