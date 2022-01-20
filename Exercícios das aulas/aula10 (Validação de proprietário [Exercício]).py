266ef73824bf0816d9149c72821f72fa5def59e3
Hugo Albert <96631538+hugoalbert03@users.noreply.github.com>
2022-01-20 00:20:07 -0300

    Exercício 01 da aula 10

---
+++ Exercícios das aulas/aula10 (Validação de proprietário [Exercício]).py
@@ -0,0 +1,7 @@
+nome = str(input("Digite o seu nome: "))
+
+if nome == "Hugo Albert":
+    print("Você é o proprietário deste computador!")
+else:
+    print("Você é um convidado deste computador!")
+print("Seja bem-vindo {}!".format(nome))
