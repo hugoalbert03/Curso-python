f110d2c0fc990afb8d819b0d0a86c78aacb4bbba
Hugo Albert <96631538+hugoalbert03@users.noreply.github.com>
2022-01-22 17:23:59 -0300

    Desafio 028

---
+++ Desafios/d028.py
@@ -0,0 +1,17 @@
+from random import randint #Importação de um elemento único do Random
+from time import sleep
+print("-=-" * 30)
+print("Pensarei em um número de 1 a 10, tente adivinhar...")
+print("-=-" * 30)
+npc = randint(1,10) #O computador sorteia um número
+nusr = int(input("Pense em um numero? ")) #O usuário pensa em um número
+print("Processando...")
+sleep(3)
+if nusr > 10 or nusr < 1: #O computador avalia se o usuário estrapolou o limite da resposta
+    print("Numeração excedida! A regra é digitar uma numeração entre 1 a 10")
+else:
+    if nusr == npc: #O computador avalia se o usuário venceu ou perdeu
+        print("Parabéns, você conseguiu me vencer!")
+    else:
+        print("Você perdeu!\nPensei em {} mas você digitou {}.".format(npc, nusr))
+        print("Tente na proxima!")
