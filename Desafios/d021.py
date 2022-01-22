4dc74f2924e7de7353779f1360589f9e8ee5cfbb
Hugo Albert <96631538+hugoalbert03@users.noreply.github.com>
2022-01-22 17:23:20 -0300

    Resolução do antigo exercício 021
    Exercício 021 resolvido!

--- Desafios/d021.py
+++ Desafios/d021.py
@@ -1,5 +1,23 @@
+#TENTATIVA 1
+import time
 import pygame
+print("processando...")
+time.sleep(3)
+print("Executando :)")
 pygame.init()
-pygame.mixer.music.load('fx01.mp3')
+pygame.mixer.music.load('audio.ogg')
 pygame.mixer.music.play()
+time.sleep(347)
 pygame.event.wait()
+
+#TENTATIVA 2
+#from playsound import playsound
+#playsound('audio.ogg')
+
+#TENTATIVA 3
+#import os
+#print(os.getcwd('audio.ogg'))
+
+#TENTATIVA 4
+#import playsound
+#playsound.playsound('audio.ogg')
