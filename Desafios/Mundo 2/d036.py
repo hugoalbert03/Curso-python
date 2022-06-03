5785dcea8245131ee79f12d6a2c3142137606b31
Hugo Albert <96631538+hugoalbert03@users.noreply.github.com>
2022-06-03 11:36:44 -0300

    Conclusão do desafio 036

--- Desafios/Mundo 2/d036.py
+++ Desafios/Mundo 2/d036.py
@@ -1,21 +1,23 @@
 from time import sleep
-print("{:\^70}".format("\033[1;36;42mEmprestimo Salarial Pra Compra de Casa\033[m"));
-valIm = float(input("\033[Qual o valor do imóvel? R$"));
-valSal = float(input("Qual é o seu salário? R$"));
-temp = int(input("Quão que tempo você estará disposto à pagar pelo imóvel?(anos) ")) ;
-print("Coletando Dados...")
+print("{:*^70}".format(" \033[1;34;42m Emprestimo Salarial Pra Compra de Casa \033[m "));
+print()
+valIm = float(input("\033[35m* Qual o valor do imóvel?\033[m \033[33mR$")); print("\033[m");
+valSal = float(input("\033[35m* Qual é o seu salário?\033[m \033[33m R$")); print("\033[m");
+temp = int(input("\033[35m* Quão que tempo você estará disposto à pagar pelo imóvel?\033[m \033[33m(anos) ")); print("\033[m");
+print("\033[33mColetando Dados...")
 sleep(2)
 print("Calculando os resultados...")
 porc = 30/100
 vPorc = porc * valSal
 parceIm = valIm / temp
 sleep(5)
-print("Analisando sua condição...")
+print("Analisando sua condição...\033[m")
 sleep(3)
+print()
 if parceIm > vPorc :
-    print("Empréstimo Negado!")
+    print("\033[1;31mEmpréstimo Negado!\033[m")
     print("Sua parcela é de R${:.2f}".format(parceIm))
     print("Porém 30% de seu salário é R${:.2f}".format(vPorc))
 else:
-    print("EMPRÉSTIMO ACEITO COM SUCESSO!")
+    print("\033[1;32mEMPRÉSTIMO ACEITO COM SUCESSO!\033[m")
     print("Boa Sorte!")
